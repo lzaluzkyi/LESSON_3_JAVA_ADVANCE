9305bf3aa4c6e583d3ba518376b8990e60a6bdb9
korandvol <60364600+korandvol@users.noreply.github.com>
2020-12-28 14:01:16 +0200

    Create СustomLoggeredFile.java

---
+++ СustomLoggeredFile.java
@@ -0,0 +1,26 @@
+package ua.lviv.lgs;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
+
+public class CustomLoggeredFile {
+	private static Logger Log = Logger.getLogger(CustomLoggeredFile.class);
+
+	public static void main(String[] args) {
+		LogWithBasicConfigurator();
+		LogWithDomConfigurator();
+
+	}
+
+	public static void LogWithBasicConfigurator() {
+		BasicConfigurator.configure();
+		Log.debug("Debug Logger of the project is on the screen!");
+		Log.info("Info Logger of the project is on the screen!");
+		Log.warn("Warn Logger of the project is on the screen!");
+	}
+	public static void LogWithDomConfigurator() {
+		DOMConfigurator.configure("loggerConfig.xml");
+		Log.error("ERROR Logger of the project is on the screen!");
+	}
+}
